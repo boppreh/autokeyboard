79659543cd530ebd8175ea7ef59e8e5d621708f9
BoppreH <boppreh@gmail.com>
2013-05-12 17:34:57 -0300

    Extract macro code to function and add speed

--- autokeyboard.py
+++ autokeyboard.py
@@ -1,27 +1,38 @@
 from keyboard import *
 
-if __name__ == '__main__':
-    actions = []
+def setup_macro(start_recording_hotkey='F7',
+                stop_recording_hotkey='F8',
+                playback_hotkey='F9',
+                playback_speed=1.0):
 
+    hotkey_keycodes = map(name_to_keycode, [start_recording_hotkey,
+                                            stop_recording_hotkey,
+                                            playback_hotkey
+                                           ])
+
+    actions = []
     def recording_handler(event):
-        if event.name not in ['f7', 'f8', 'f9']:
+        if event.keycode not in hotkey_keycodes:
             actions.append(event)
 
     def start_recording():
         actions[:] = []
-        print 'Recording...'
         add_handler(recording_handler)
 
     def stop_recording():
-        print 'Recorded {} actions.'.format(len(actions))
         remove_handler(recording_handler)
 
     def playback():
-        print 'Playing {} actions.'.format(len(actions))
-        play(list(actions))
-        print 'Finished playing.'
+        if playback_speed > 0:
+            play(list(actions), playback_speed)
+        else:
+            send_keys(action.keycode for action in actions
+                      if action.event_type == KEY_DOWN)
 
-    register_hotkey('F7', start_recording)
-    register_hotkey('F8', stop_recording)
-    register_hotkey('F9', playback)
+    register_hotkey(start_recording_hotkey, start_recording)
+    register_hotkey(stop_recording_hotkey, stop_recording)
+    register_hotkey(playback_hotkey, playback)
+
+if __name__ == '__main__':
+    setup_macro(playback_speed=0)
     raw_input()
